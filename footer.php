0b9446211b36199986d5d0da2141bd56349da06e
Majid Shahbazkhani <hipcats@yahoo.com>
2014-05-31 05:17:35 -0500

    clean bones

---
+++ footer.php
@@ -0,0 +1,20 @@
+			<footer class="footer" role="contentinfo">
+
+				<div id="inner-footer" class="wrap cf">
+
+					
+
+					<p class="source-org copyright">&copy; <?php echo date('Y'); ?> Majid Shahbazkhani.</p>
+
+				</div>
+
+			</footer>
+
+		</div>
+
+		<?php // all js scripts are loaded in library/bones.php ?>
+		<?php wp_footer(); ?>
+
+	</body>
+
+</html> <!-- end of site. what a ride! -->

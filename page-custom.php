811c4a12b9f9850ffc09840ddbc2797e773aa94f
Majid Shahbazkhani <hipcats@yahoo.com>
2014-06-07 02:35:41 -0500

    in clean branch but changed vr2

--- page-custom.php
+++ page-custom.php
@@ -16,7 +16,7 @@
 <?php get_header(); ?>
 
 			<div id="content">
-Chenge page-custom.php to cearte a new template
+vr2-page-custom.php 
 				<div id="inner-content" class="wrap cf">
 
 						<div id="main" class="m-all t-2of3 d-5of7 cf" role="main">
